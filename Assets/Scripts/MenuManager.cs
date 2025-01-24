3bd0bfba3002c04c1e1b66bc8be970f0838ed765
Vilmokas <vilmutizzz@gmail.com>
2025-01-24 21:30:25 +0200

    add main menu scene & menu manager script

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField] GameObject creditsWindow;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void ShowCreditsWindow()
+    {
+        creditsWindow.SetActive(true);
+    }
+
+    public void HideCreditsWindow()
+    {
+        creditsWindow.SetActive(false);
+    }
+
+    public void CloseGame()
+    {
+        Application.Quit();
+    }
+}
