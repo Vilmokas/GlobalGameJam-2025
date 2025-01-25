a39c815643646cce17f1e486cb5e9c549c953fa1
Vilmokas <vilmutizzz@gmail.com>
2025-01-25 15:47:42 +0200

    add currency drops from enemies

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,6 +4,7 @@ public class EnemyController : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 5f;
     [SerializeField] float nextPointThreshold = 0.1f;
+    [SerializeField] int pointsToGiveOnDeath = 1;
     public Transform[] pathPoints;
     int currentPathPoint = 0;
 
@@ -16,5 +17,11 @@ public class EnemyController : MonoBehaviour
     {
         transform.Translate((pathPoints[currentPathPoint].position - transform.position).normalized * moveSpeed * Time.deltaTime);
         if (Vector2.Distance(transform.position, pathPoints[currentPathPoint].position) <= nextPointThreshold) currentPathPoint++;
+    }
+
+    public void DestroyEnemy()
+    {
+        GameObject.Find("TurretSpawner").GetComponent<TurretSpawner>().AddCurrency(pointsToGiveOnDeath);
+        Destroy(gameObject);
     }
 }
--- Assets/Scripts/TurretSpawner.cs
+++ Assets/Scripts/TurretSpawner.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 
 public class TurretSpawner : MonoBehaviour
@@ -5,11 +6,24 @@ public class TurretSpawner : MonoBehaviour
     [SerializeField] GameObject[] turretPrefabs;
     [SerializeField] int[] turretCosts;
     [SerializeField] int currency = 0;
+    [SerializeField] TextMeshProUGUI currencyText;
+
+    private void Start()
+    {
+        currencyText.text = currency.ToString();
+    }
 
     public void SpawnTurret(int turretId)
     {
         if (turretCosts[turretId] > currency) return;
         currency -= turretCosts[turretId];
+        currencyText.text = currency.ToString();
         Instantiate(turretPrefabs[turretId]);
+    }
+
+    public void AddCurrency(int newCurrency)
+    {
+        currency += newCurrency;
+        currencyText.text = currency.ToString();
     }
 }
